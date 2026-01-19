3619d6cf41d8fb101ce103eb779b1d43b4579fa9
justshev <mshevaatallah@gmail.com>
2026-01-19 20:10:45 +0700

    fix redirect user register

--- hooks/auth/use-register.tsx
+++ hooks/auth/use-register.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { useFormik } from "formik";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { useRouter } from "next/navigation";
 import { toast } from "sonner";
 
 import { getErrorMessage } from "@/utils/error";
@@ -11,12 +12,14 @@ import { registerSchema } from "@/features/schema/auth.schema";
 const useRegister = () => {
   const [showPassword, setShowPassword] = useState(false);
   const queryClient = useQueryClient();
+  const router = useRouter();
 
   const { mutate, isPending } = useMutation({
     mutationFn: registerRequest,
     onSuccess: async (data) => {
       toast.success(data.message || "Registrasi berhasil");
       await queryClient.invalidateQueries({ queryKey: ["auth", "me"] });
+      router.push("/login");
     },
     onError: (error) => {
       const message = getErrorMessage(error, "Registrasi gagal");
